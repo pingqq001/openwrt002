6f28fb1ff1c8a055dcb9b998838e170b58edaf7d
pingqq001 <105734691+pingqq001@users.noreply.github.com>
2023-04-22 00:18:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,10 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git istore https://github.com/luochongjun/istore.git' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/openclash.git' >>feeds.conf.default
+
+
