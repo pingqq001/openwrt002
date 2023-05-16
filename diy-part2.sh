f4c00c908ab7a38d97b0551bd4546166eb5eae1d
pingqq001 <105734691+pingqq001@users.noreply.github.com>
2023-05-16 18:40:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+rm -rf luci-theme-argon
+
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
